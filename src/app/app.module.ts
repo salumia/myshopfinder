881804504683f71eb1b7d0a3220fa4193c89aefb
MukeshTrigvent <trigventmk@gmail.com>
2021-07-03 11:09:40 +0530

    Update Before Contact 3 July 2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {ListboxModule} from 'primeng/listbox';
 import {SliderModule} from 'primeng/slider';
 import {InputSwitchModule} from 'primeng/inputswitch';
 import {ScrollPanelModule} from 'primeng/scrollpanel';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {ScrollPanelModule} from 'primeng/scrollpanel';
     HomeComponent,
     FeatureComponent,
     ProductsComponent,
-    CategoryProductComponent
+    CategoryProductComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
