83016b314a2f9115154eead5029bcf570feb6d1c
MukeshTrigvent <trigventmk@gmail.com>
2021-07-21 12:55:20 +0530

    Updated Meta Description on 21 July 2021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   BASE_API_URL: "https://myshopfinder.com/admin/api/",
   BASE_TITLE:"Shop the best fashion in one place | Shopping made easy | MyShopFinder",
-  BASE_DESCRIPTION:"Shop thousands of products from top brands in one place. Get the latest prices, and best trending items | MyShopFinder",
+  BASE_DESCRIPTION:"Shop the best fashion in one place. Carefully hand selected brands and the best products and prices only",
   COMMON_TITLE:" | Shop the best fashion in one place | MyShopFinder",
   LIMIT_RECORD:16
 };
