1dc5625aab4ab4b9969c9128370244f06a3a46ea
Mukesh Kumar <trigventmk@gmail.com>
2021-08-19 09:34:05 +0530

    Environment Updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  BASE_API_URL: "http://localhost/admin/api/",
-  BASE_API_URL_LIVE: "https://myshopfinder.com/admin/api/",
+  BASE_API_URL_LOCAL: "http://localhost/admin/api/",
+  BASE_API_URL: "https://myshopfinder.com/admin/api/",
   BASE_TITLE:"Shop the best fashion in one place | Shopping made easy | MyShopFinder",
   BASE_DESCRIPTION:"Shop the best fashion in one place. Carefully hand selected brands and the best products and prices only",
   COMMON_TITLE:" | Shop the best fashion in one place | MyShopFinder",
